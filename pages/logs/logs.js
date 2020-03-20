21a92b8bce81c2edcc09b6382ca55ac88d791193
汪洁 <15151135869@163.com>
2020-03-20 11:11:07 +0800

    学习笔记

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,30 @@
 //logs.js
 const util = require('../../utils/util.js')
-
+//注册页面
 Page({
+  //------------2.初始化数据---------
   data: {
     logs: []
   },
+  //---------------1.监听页面的生命周期函数-------------
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
     })
+  },
+  //------------3.监听事件--------------
+  click(){
+     console.log('被点击了')
+  },
+  // --------------4.其他事件---------------
+  //监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
+  },
+  //下拉刷新事件
+  onPullDownRefresh(){
+    console.log('下拉刷新的事件')
   }
 })
