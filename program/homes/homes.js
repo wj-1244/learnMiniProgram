bb141a1b692eaee1964a98a5f089d6c23feae1ca
汪洁 <15151135869@163.com>
2020-03-25 17:52:43 +0800

    商城项目练习

---
+++ program/homes/homes.js
@@ -0,0 +1,63 @@
+// program/homes/homes.js
+import { request } from "../../programrequest/request.js"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 轮播图数组
+    swiperList:[],
+    // 导航数组
+    catesList:[],
+    // 楼层数组
+    floorList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //  发送异步请求获取轮播图数据
+    // wx-wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   method: 'GET',
+    //   success: (res) => {
+    //     console.log(res)
+    //     this.setData({
+    //       swiperList:res.data.message
+    //     })
+    //   },
+    //   fail: function(res) {},
+    //   complete: function(res) {},
+    // })
+     this.getSwiperList();
+    this.getCateList();
+    this.getFloorList()
+  },
+  getSwiperList(){
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata" })
+     .then(res => {
+        this.setData({
+          swiperList: res.data.message
+        })
+     })
+  },
+  getFloorList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata" })
+      .then(res => {
+        this.setData({
+          floorList: res.data.message
+        })
+      })
+  },
+  getCateList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems" })
+      .then(res => {
+        this.setData({
+          catesList: res.data.message
+        })
+      })
+  },
+ 
+})
